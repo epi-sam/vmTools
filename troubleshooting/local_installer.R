603cc058d2509d532c28e1d8c20a72f4dce64dd4
Sam Byrne <ssbyrne@uw.edu>
2024-03-07 14:24:10 -0800

    update installer script

--- troubleshooting/local_installer.R
+++ troubleshooting/local_installer.R
@@ -3,7 +3,7 @@ library(devtools)
 with_libpaths(
 
    new = "/mnt/share/code/ssbyrne/r_pkgs/"
-   , code =  devtools::install_local(path = "~/rstudio/projects/vmTools"))
+   , code =  devtools::install_local(path = "~/rstudio/projects/vmTools"
 
    )
 )
