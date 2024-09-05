accd7989465d857de5e596f1c1fb68b9134fd63d
Sam Byrne <ssbyrne@uw.edu>
2024-09-05 09:38:09 -0700

    dependency removed - DescTools

--- R/symlink_tool.R
+++ R/symlink_tool.R
@@ -54,6 +54,11 @@
 #       - clutters up the logs, but not a deal-breaker
 # TODO SB - 2024 Sep 03
 # - [ ] Remove dependency on all packags except R6
+#     - [x] library(DescTools)
+#     - [ ] library(data.table)
+#        - [ ] replace with library(readr) to avoid quoting issues
+#     - [ ] library(lubridate)
+#     - [ ] library(purr)
 #     - [ ] lubridate will make this tricky
 # - [ ] Move private method docstrings inside functions so they can be seen when called anonymously
 # - [ ] Convert messages to std_out?
@@ -66,6 +71,7 @@
 #> library(DescTools)
 #> library(data.table)
 #> library(lubridate)
+#> library(purr)
 #>
 #> WARNING: DO NOT touch '.__enclos_env__' unless you want the tool to break
 #>
@@ -570,6 +576,115 @@ SLT <- R6::R6Class(
          (inherits(x, "simpleError") | inherits(x, "try-error"))
       },
 
+      ## DescTools -------------------------------------------------------------
+
+      ## Functions borrowed from DescTools to remove dependency
+
+      BlankIfNA = function (x, blank = "") {
+         #' Replace NAs by 0
+         #'
+         #' DescTools::BlankIfNA
+         #'
+         #' Replace NAs in a numeric vector x with 0. This function has the same
+         #' logic as the zeroifnull function in SQL. NAIfZero() does replace zeros
+         #' with NA. BlankIfNA() and NAIfBlank() do the same, but for character
+         #' vectors.
+         #'
+         #' @param x [vector] whose NAs should be overwritten with 0s.
+         #' @param blank [chr] the value to replace NAs with.
+         #'
+         #' @returnthe [vector] edited vector x
+         replace(x, is.na(x), blank)
+      },
+
+      ZeroIfNA <- function (x) {
+         #' Replace NAs by 0
+         #'
+         #' DescTools::ZeroIfNA
+         #'
+         #' Replace NAs in a numeric vector x with 0. This function has the same
+         #' logic as the zeroifnull function in SQL. NAIfZero() does replace zeros
+         #' with NA. BlankIfNA() and NAIfBlank() do the same, but for character
+         #' vectors.
+         #'
+         #' @param x [vector] whose NAs should be overwritten with 0s.
+         #'
+         #' @return [vector] edited vector x
+         replace(x, is.na(x), 0L)
+      },
+
+      StrExtract <- function (x, pattern, ...) {
+         #' Extract Part of a String
+         #'
+         #' DescTools::StrExtract
+         #'
+         #' Extract a part of a string, defined as regular expression.
+         #' StrExtractBetween() is a convenience function used to extract parts
+         #' between a left and right delimiter.
+         #'
+         #' @param x [chr] vector where matches are sought, or an object which can
+         #'   be coerced by as.character to a character vector.
+         #' @param pattern [chr]  string containing a regular expression (or
+         #'   character string for fixed = TRUE) to be matched in the given
+         #'   character vector. Coerced by as.character to a character string if
+         #'   possible. If a character vector of length 2 or more is supplied, the
+         #'   first element is used with a warning. Missing values are not allowed.
+         #' @param ... the dots are passed to the the internally used function
+         #'   regexpr(), which allows to use e.g. Perl-like regular expressions.
+         #'
+         #' @return [chr] A character vector.
+         m                    <- regexpr(pattern, x, ...)
+         regmatches(x, m)
+         res                  <- rep(NA_character_, length(m))
+         res[ZeroIfNA(m) > 0] <- regmatches(x, m)
+         return(res)
+      },
+
+
+      split_path = function(path, last.is.file = NULL) {
+         #' Split Path In Drive, Path, Filename
+         #'
+         #' DescTools::split_path
+         #'
+         #' Split a full path in its components. This is specifically an issue in
+         #' Windows and not really interesting for other OSs.
+         #'
+         #' @param path [chr] a path
+         #' @param last.is.file [lgl] logical, determining if the basename should be
+         #' interpreted as filename or as last directory. If set to NULL
+         #' (default), the last entry will be interpreted if the last character is
+         #' either \ or / and as filename else.
+         if (is.null(last.is.file)) {
+            last.is.file <- (length(grep(pattern = "[/\\]$", path)) ==
+                                0)
+         }
+         path <- normalizePath(path, mustWork = FALSE)
+
+
+         if (.Platform$OS.type == "windows") {
+            lst$drive   <- regmatches(path, regexpr("^([[:alpha:]]:)|(\\\\[[:alnum:]]+)",path))
+            lst$dirname <- gsub(pattern = lst$drive, x = dirname(path), replacement = "")
+         } else {
+            lst$drive   <- NA
+            lst$dirname <- dirname(path)
+         }
+
+         lst              <- list()
+         lst$normpath     <- path
+         lst$dirname      <- paste(lst$dirname, "/", sep = "")
+         lst$fullfilename <- basename(path)
+         lst$fullpath     <- paste0(BlankIfNA(lst$drive), lst$dirname)
+         lst$filename     <- gsub(pattern = "(.*)\\.(.*)$", "\\1", lst$fullfilename)
+         lst$extension    <- StrExtract(pattern = "(?<=\\.)[^\\.]+$", lst$fullfilename, perl = TRUE)
+
+         if (!last.is.file) {
+            lst$dirname <- paste(lst$dirname, lst$fullfilename, "/", sep = "")
+            lst$extension <- lst$filename <- lst$fullfilename <- NA
+         }
+
+         return(lst)
+      },
+
 
 
       # Implementations --------------------------------------------------------
@@ -708,9 +823,9 @@ SLT <- R6::R6Class(
          private$assert_scalar(version_path)
          private$assert_scalar(symlink_type)
          if(!symlink_type %in% private$DICT$symlink_types) stop("Invalid symlink_type: ", symlink_type)
-         root         <- DescTools::SplitPath(version_path)$dirname
+         root         <- private$SplitPath(version_path)$dirname
          root         <- sub("/$", "", root) # remove trailing slash
-         date_version <- DescTools::SplitPath(version_path)$fullfilename
+         date_version <- private$SplitPath(version_path)$fullfilename
 
          symlink_suffix <- switch(
             symlink_type,
@@ -2073,13 +2188,13 @@ SLT <- R6::R6Class(
                  list( input_root = '/mnt/share/my_team/input_data',
                       output_root = '/mnt/share/my_team/output_data' ) \n\n  ",
 
-                 "This tool assumes each root has a matching `date_version` output folder. \n  ",
-                 "  You may divert outputs to one root, or many roots in parallel. \n\n  ",
+                    "This tool assumes each root has a matching `date_version` output folder. \n  ",
+                    "  You may divert outputs to one root, or many roots in parallel. \n\n  ",
 
-                 "Each output folder defined by `file.path(user_root_list, date_version)`. \n  ",
-                 "  The `date_version` is defined when the user wants to 'mark' or 'unmark' a folder as best/keep/remove. \n  ",
-                 "  This folder receives a log of all *demotion* and *promotion* actions (marking and unmarking). \n  ",
-                 "  This `date_version` log is used for report generation. \n\n  "
+                    "Each output folder defined by `file.path(user_root_list, date_version)`. \n  ",
+                    "  The `date_version` is defined when the user wants to 'mark' or 'unmark' a folder as best/keep/remove. \n  ",
+                    "  This folder receives a log of all *demotion* and *promotion* actions (marking and unmarking). \n  ",
+                    "  This `date_version` log is used for report generation. \n\n  "
             )
          }
 
@@ -2089,14 +2204,14 @@ SLT <- R6::R6Class(
                     "e.g.
                  '/mnt/share/my_team' \n\n  ",
 
-                 "The central log is a summary record of all *promotion* (marking) actions done by this tool, \n  ",
-                 "  but is not used for report generation. \n\n  ",
-                 "The central log is *created* on initialization i.e. when calling `SLT$new()`. \n\n  ",
+                    "The central log is a summary record of all *promotion* (marking) actions done by this tool, \n  ",
+                    "  but is not used for report generation. \n\n  ",
+                    "The central log is *created* on initialization i.e. when calling `SLT$new()`. \n\n  ",
 
-                 "Each output folder defined by `file.path(user_root_list, date_version)`. \n  ",
-                 "  The `date_version` is defined when the user wants to 'mark' or 'unmark' a folder as best/keep/remove. \n  ",
-                 "  This folder receives a log of all *demotion* and *promotion* actions (marking and unmarking). \n  ",
-                 "  This `date_version` log is used for report generation. \n\n  "
+                    "Each output folder defined by `file.path(user_root_list, date_version)`. \n  ",
+                    "  The `date_version` is defined when the user wants to 'mark' or 'unmark' a folder as best/keep/remove. \n  ",
+                    "  This folder receives a log of all *demotion* and *promotion* actions (marking and unmarking). \n  ",
+                    "  This `date_version` log is used for report generation. \n\n  "
             )
          }
 
