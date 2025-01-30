b849272a457e98474cb8aa8d2fd874e362007256
Sam Byrne <ssbyrne@uw.edu>
2025-01-30 12:46:55 -0800

    typos

--- tests/testthat/test-lapply_depth.R
+++ tests/testthat/test-lapply_depth.R
@@ -1,4 +1,4 @@
-test_that("lapply_depth plucking", {
+test_that("lapply_depth plucking works", {
   expect_equal(
      lapply_depth(list(A = list(one = 1, two = 2), B = list(one = 3, two = 4)), 1, `[[`, "one")
      , list(A = 1, B = 3)
