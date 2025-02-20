80590c0ac1112e0d5c8c1032047e6f7613572fc9
Sam Byrne <ssbyrne@uw.edu>
2025-02-20 13:55:50 -0800

    removed all lingering util dependencies

---
+++ R/split_line_breaks.R
@@ -0,0 +1,17 @@
+# started: 2025 Jan 31 13:34:16
+# purpose: assist vignette displays
+
+#' Split a character vector by line breaks
+#'
+#' @param string [chr] A character vector.
+#'
+#' @return All elements of the character vector are split by `\\n` into new elements.
+#'
+#' @examples
+#' vmTools:::split_line_breaks(c('a', 'b\nc'))
+split_line_breaks = function(string) {
+   if (length(grep('\n', string)) == 0L) return(string)
+   string = gsub('\n$', '\n\n', string)
+   string[string == ''] = '\n'
+   unlist(strsplit(string, '\r?\n'))
+}
