e4d6cbb0322712c87469ec20e25358ee4dba694f
Sam Byrne <ssbyrne@uw.edu>
2025-02-20 13:25:23 -0800

    split_path - replaced with base R functions, tests passing windows

--- tests/testthat/test-symlink_tool.R
+++ tests/testthat/test-symlink_tool.R
@@ -29,82 +29,88 @@ ue_list <- list(
 )
 
 
-# Create tool ------------------------------------------------------------------
-
-test_that(
-   "Naive startup produces messages and error",
-   {
-      expect_message(
-         expect_message(
-            expect_error(
-               SLT$new(), regexp = "You must provide both user_root_list and user_central_log_root"
-            ) , regexp = "This tool expects `user_central_log_root` to be a single directory for the central log"
-         ) , regexp = "This tool expects \\`user_root_list\\` to be a named list of root directories for pipeline outputs"
-      )
-
-   }
-)
-
-# Create a real tool with two roots
-# - doing integration tests with multiple roots to really torture test things
-suppressMessages(
-   suppressWarnings({ # idiosyncratic and benign cluster message
-
-      slt <- SLT$new(
-         user_root_list = list(
-            root_input  = root_list[["root_input"]],
-            root_output = root_list[["root_output"]]
-         )
-         , user_central_log_root = root_base
-      )
-   })
-)
-
-
-
-
-# Paths ------------------------------------------------------------------------
-
-# file names used in tests - draw from within tool to ensure consistency
-fname_dv_log <- slt$return_dictionaries()$log_name
-fname_report_key_versions <- slt$return_dictionaries()$report_fnames$all_logs_tool_symlink
-fname_discrepnacy_report <- slt$return_dictionaries()$report_fnames$discrepancies
-
-fpaths_dv_logs <- lapply(path_list, function(path) file.path(path, fname_dv_log))
-fpaths_dv_logs <- lapply(fpaths_dv_logs, function(x) {names(x) <- names(dv_list); x})
-
-
-
-
-
 # Integration Tests ------------------------------------------------------------
 
+# All integration tests will fail on Windows unless the R process runs with
+# administrative privileges, which is not possible on CRAN servers.
+# - Right click on Rstudio > "Run as administrator" > Yes
+#
+# See description section of ?base::file.symlink.
+#
+# Symbolic links on Windows
+# Symbolic links in the sense of POSIX file systems do not exist on Windows:
+# however, NTFS file systems support two similar concepts.
+#
+# ...
+#
+# A version of symbolic linking to files/directories was implemented more
+# recently, and file.symlink makes use of that interface. However, it has
+# restrictions which are crippling. First, the user needs permission to make
+# symbolic links, and that permission is not normally granted except to
+# Administrator accounts (note: not users with Administrator rights): further
+# many users report that whereas the Policy Editor appears to be able to grant
+# such rights, the API still reports insufficient permissions. Second, the
+# interface needs to know if from is a file or a directory (and it need not yet
+# exist): we have implemented this to allow linking from a directory only if it
+# currently exists.
+
 if(tolower(.Platform$OS.type) == "windows" & vmTools:::is_windows_admin() == FALSE){
+
+   test_that(
+      "Windows non-admin startup produces correct error",
+      {
+         expect_error(
+            SLT$new(), regexp = "Symbolic links are not supported on Windows without admin privileges."
+         )
+      }
+   )
+
    testthat::skip("Symbolic links are not supported on Windows without admin privileges")
+
 } else {
 
-   # All integration tests will fail on Windows unless the R process runs with
-   # administrative privileges, which is not possible on CRAN servers.
-   # - Right click on Rstudio > "Run as administrator" > Yes
-   #
-   # See description section of ?base::file.symlink.
-   #
-   # Symbolic links on Windows
-   # Symbolic links in the sense of POSIX file systems do not exist on Windows:
-   # however, NTFS file systems support two similar concepts.
-   #
-   # ...
-   #
-   # A version of symbolic linking to files/directories was implemented more
-   # recently, and file.symlink makes use of that interface. However, it has
-   # restrictions which are crippling. First, the user needs permission to make
-   # symbolic links, and that permission is not normally granted except to
-   # Administrator accounts (note: not users with Administrator rights): further
-   # many users report that whereas the Policy Editor appears to be able to grant
-   # such rights, the API still reports insufficient permissions. Second, the
-   # interface needs to know if from is a file or a directory (and it need not yet
-   # exist): we have implemented this to allow linking from a directory only if it
-   # currently exists.
+   # Create tool ------------------------------------------------------------------
+
+   test_that(
+      "Naive startup produces messages and error",
+      {
+         expect_message(
+            expect_message(
+               expect_error(
+                  SLT$new(), regexp = "You must provide both user_root_list and user_central_log_root"
+               ) , regexp = "This tool expects `user_central_log_root` to be a single directory for the central log"
+            ) , regexp = "This tool expects \\`user_root_list\\` to be a named list of root directories for pipeline outputs"
+         )
+
+      }
+   )
+
+   # Create a real tool with two roots
+   # - doing integration tests with multiple roots to really torture test things
+   suppressMessages(
+      suppressWarnings({ # idiosyncratic and benign cluster message
+
+         slt <- SLT$new(
+            user_root_list = list(
+               root_input  = root_list[["root_input"]],
+               root_output = root_list[["root_output"]]
+            )
+            , user_central_log_root = root_base
+         )
+      })
+   )
+
+
+   # Paths ------------------------------------------------------------------------
+
+   # file names used in tests - draw from within tool to ensure consistency
+   fname_dv_log <- slt$return_dictionaries()$log_name
+   fname_report_key_versions <- slt$return_dictionaries()$report_fnames$all_logs_tool_symlink
+   fname_discrepnacy_report <- slt$return_dictionaries()$report_fnames$discrepancies
+
+   fpaths_dv_logs <- lapply(path_list, function(path) file.path(path, fname_dv_log))
+   fpaths_dv_logs <- lapply(fpaths_dv_logs, function(x) {names(x) <- names(dv_list); x})
+
 
    # Integration - Make new folders -----------------------------------------------
 
