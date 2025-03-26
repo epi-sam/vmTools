83a79247a22b0378408367d27b82242dd2bd75d6
Sam Byrne <ssbyrne@uw.edu>
2025-03-26 09:04:21 -0700

    tests - started dir_tree test

---
+++ tests/testthat/test-dir_tree.R
@@ -0,0 +1,16 @@
+
+test_that("dir_tree produces correct top-level repo file structure and formatting", {
+   expect_equal(
+      sort(capture.output(dir_tree(path = "fixtures")))
+      , sort(c("`-- \033[1;35mversioned-dirs\033[0m",
+               "   `-- \033[1;35mnested\033[0m",
+               "      `-- \033[1;35m1999_09_09\033[0m",
+               "         |-- \033[1;35m1999_09_09.01\033[0m",
+               "         |  `-- \033[32mdata.txt\033[0m",
+               "         |-- \033[1;35m1999_09_09.02\033[0m",
+               "         |  `-- \033[32mdata.txt\033[0m",
+               "         `-- \033[1;35m1999_09_09.NA\033[0m",
+               "            `-- \033[32mQC_1999_09_09.01\033[0m"))
+   )
+})
+
